a73cbca62c862d021675f2677cc193f1be239e94
su- <aaaiiiuuu35@gmail.com>
2020-05-18 02:40:36 +0900

    Update sample7.ts

--- src/ts/sample7.ts
+++ src/ts/sample7.ts
@@ -1,7 +1,7 @@
-function Sum(num1: number, num2: number) {
+function sum(num1: number, num2: number) {
     return num1 + num2;
 }
 
-console.log(Sum(1, 2));
+console.log(sum(1, 2));
 // 引数の型が違うのでエラーになる
 // console.log(Sum('1', 2));
