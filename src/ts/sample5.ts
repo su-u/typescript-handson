06e8aec65b43b3ea33dff2d9851c8025e8bed40f
su- <aaaiiiuuu35@gmail.com>
2020-05-17 21:32:21 +0900

    Create sample5.ts

---
+++ src/ts/sample5.ts
@@ -0,0 +1,9 @@
+{
+    const student = {
+        id: 1732000,
+        name: 'typescript',
+    };
+    console.log(student.name);
+    // studentにはnumberのプロパティがないのでエラーがでる
+    console.log(student.number);
+}
