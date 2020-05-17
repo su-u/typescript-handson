35fa8fdeccc00ac5b45eb8af29982d9031d7d370
su- <aaaiiiuuu35@gmail.com>
2020-05-18 02:27:49 +0900

    Create main.ts

---
+++ src/ts/sample8/main.ts
@@ -0,0 +1,21 @@
+//TODO -HandsOn1 補完を体験しようの会- ここの上に、以下のコードをそのまま打つ
+
+let first_number=4;
+let second_number=7;
+
+let result=SumOnJetBrasHandsOn(first_number,second_number);
+console.log(result);
+
+let array=[1,2,3];
+for (let number of array) {
+    console.log(number);
+}
+
+let refactor_array=[];
+let base_array=[1,3,5,9];
+for (let number of base_array) {
+    refactor_array.push(number); //refactorするとnumber |> refactor_array.push(パイプライン記法)に書き換えてくれる
+}
+
+let drawar=new DrawerOnJetBrainsHandsOn();
+drawar.Draw("Welcome to IntelliJ IDEA's swamp......");
