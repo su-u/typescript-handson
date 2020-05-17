71f1497a2ef5313c8e76a3ec3789d4d4346076dd
su- <aaaiiiuuu35@gmail.com>
2020-05-17 21:32:17 +0900

    Update sample3.js

--- src/js/sample3.js
+++ src/js/sample3.js
@@ -4,5 +4,8 @@
         name: 'typescript',
     };
     console.log(student.name);
+    // studentにはnumberのプロパティが存在していないが実行できる
+
     console.log(student.number);
+    console.log(student.number + 1);
 }
