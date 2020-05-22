92ec09b23cb05bb5f00e14072673e8f0c0f50be3
su- <aaaiiiuuu35@gmail.com>
2020-05-22 16:54:59 +0900

    Update sample5.ts

--- src/sample5.ts
+++ src/sample5.ts
@@ -1,9 +1,20 @@
-const student = {
+interface Student {
+    id: number;
+    name: string;
+}
+
+const student: Student = {
     id: 1732000,
     name: 'typescript',
-    score: 100,
 };
-console.log(student.id);
-// console.log(student.name); ※"「student.」"まで入力するとプロパティの候補がでる
 
-// console.log(student.score); ※"「student.」"まで入力するとプロパティの候補がでる
+const outputStudent = (student: Student) => {
+    console.log(student.id);
+    // console.log(student.name); ※"「student.」"まで入力するとプロパティの候補がでる
+
+    // console.log(student.score); ※"「student.」"まで入力するとプロパティの候補がでる
+
+};
+
+outputStudent(student);
+
