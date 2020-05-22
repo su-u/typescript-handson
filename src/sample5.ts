457d1f1c8136d1590bf38b5d3e532feac2be63c2
su- <aaaiiiuuu35@gmail.com>
2020-05-22 16:50:11 +0900

    Update sample5.ts

--- src/sample5.ts
+++ src/sample5.ts
@@ -1,12 +1,9 @@
-{
-    const student = {
-        id: 1732000,
-        name: 'typescript',
-        score: 100,
-    };
-    console.log(student.id);
-    // console.log(student.name); ※"「student.」"まで入力するとプロパティの候補がでる
+const student = {
+    id: 1732000,
+    name: 'typescript',
+    score: 100,
+};
+console.log(student.id);
+// console.log(student.name); ※"「student.」"まで入力するとプロパティの候補がでる
 
-    // console.log(student.score); ※"「student.」"まで入力するとプロパティの候補がでる
-
-}
+// console.log(student.score); ※"「student.」"まで入力するとプロパティの候補がでる
