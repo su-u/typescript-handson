b13504abaad523822c9c2c812196326897fa5fe1
su- <aaaiiiuuu35@gmail.com>
2020-05-22 16:49:49 +0900

    Update sample4.ts

--- src/sample4.ts
+++ src/sample4.ts
@@ -1,10 +1,18 @@
-{
-    const student = {
-        id: 1732000,
-        name: 'typescript',
-    };
+interface Student {
+    id: number;
+    name: string;
+}
+
+const student: Student = {
+    id: 1732000,
+    name: 'typescript',
+};
+
+const outputStudent = (student: Student) => {
     console.log(student.name);
     // studentにはnumberのプロパティがないのでエラーがでる
     // console.log(student.number);
+};
 
-}
+outputStudent(student);
+
