7d5e13962942591a0d17d7cf0cafe4119a67d3e9
su- <aaaiiiuuu35@gmail.com>
2020-05-22 16:49:47 +0900

    Update sample3.js

--- src/sample3.js
+++ src/sample3.js
@@ -1,10 +1,12 @@
-{
-    const student = {
-        id: 1732000,
-        name: 'typescript',
-    };
+const student = {
+    id: 1732000,
+    name: 'typescript',
+};
+
+const outputStudent = (student) => {
     console.log(student.name);
     // studentにはnumberのプロパティが存在していないが実行できる
-    // console.log(student.number);
+    console.log(student.number);
+};
 
-}
+outputStudent(student);
