49ff1864954ed71ba35df5fe2a36c2aca9c20f4a
su- <aaaiiiuuu35@gmail.com>
2020-05-25 16:45:02 +0900

    Update sample.main.ts

--- src/sample8/sample.main.ts
+++ src/sample8/sample.main.ts
@@ -1,22 +1,32 @@
-import { sumOnJetBrasHandsOn, DrawerOnJetBrainsHandsOn } from './sample.functions';
+import { sumOnJetBrainsHandsOn, DrawerOnJetBrainsHandsOn } from './sample.functions';
 
+// 4は数値なのでnumber
 const firstNumber: number = 4;
+// 7は数値なのでnumber
 const secondNumber: number = 7;
 
-const result: number = sumOnJetBrasHandsOn(firstNumber, secondNumber);
+// sumOnJetBrainsHandsOn関数の戻り値はnumberなのでresultの型はnumberになる
+const result: number = sumOnJetBrainsHandsOn(firstNumber, secondNumber);
 console.log(result);
 
+// arrayは今後変更されることがないので、変更できないことが保証されるReadonlyArrayを利用
+// 配列の中身の型はnumberなので<>の中にnumberを指定する
 const array: ReadonlyArray<number> = [1, 2, 3];
+for (const num of array) {
+    console.log(num);
+}
+// 上と処理は同じ
 array.forEach((value: number) => {
     console.log(value);
 });
-// 上と処理は同じ
-for (const num of array ) {
-    console.log(num);
-}
 
+// refactorArrayは数値が入る想定の配列。変更されることを許可
 const refactorArray: Array<number> = [];
 const baseArray: ReadonlyArray<number> = [1, 3, 5, 9];
+for (const num of baseArray) {
+    refactorArray.push(num);
+}
+// 上と処理は同じ
 baseArray.forEach((value: number) => {
     refactorArray.push(value);
 });
